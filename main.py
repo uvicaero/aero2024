aa94fe53aea3cc23b40ddc807edc6b7cedab40f1
Brent Machado <brentmachado7@gmail.com>
2024-11-02 14:09:59 -0700

    fixed timestamps and printing centroids

--- main.py
+++ main.py
@@ -4,20 +4,21 @@ from picamzero import Camera
 import sys
 sys.path.append("./utils")
 
-import detect_hotspots
+from detect_hotspots import detect_hotspots
 import time
+from datetime import datetime
 
 cam = Camera()
 #cam.start_preview()
 
 try:
     while True:
-        timestamp = int(time.time())
+        timestamp = datetime.fromtimestamp(int(time.time())).strftime('%Y-%m-%d %H:%M:%S')
         path = f"./photos/photo_{timestamp}.jpg"
         cam.take_photo(path)
         print(f"\nPhoto taken: {path}")
         
-        detect_hotspots(path, 0.9, debug=True)
+        detect_hotspots(path, 0.9, debug=False)
         
         time.sleep(3)
 except KeyboardInterrupt:
