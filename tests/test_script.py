9881720e8479b6dfd09ac0464c4158c6358d5f5f
Ben <benjaminparker@uvic.ca>
2024-12-16 16:51:07 -0800

    Changed the port connection in test_script.py

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,11 +1,12 @@
 
 from pymavlink import mavutil
-#from src.functions.mav_utils import get_latest_gps, send_waypoint, send_waypoints, set_auto_mode
+from src.functions.mav_utils import get_latest_gps, send_waypoint, send_waypoints, switch_to_auto
+import time
 
 def run():
     print("Running...")
     # connect to SITL over tcp:127.0.0.1:5762
-    connection = mavutil.mavlink_connection('tcp:127.0.0.1:5762')
+    connection = mavutil.mavlink_connection('tcp:127.0.0.1:5763')
 
     # wait for a heartbeat
     connection.wait_heartbeat()
@@ -15,6 +16,19 @@ def run():
 
     print("Connected: ")
     print(connection)
+    while True:
+        print(get_latest_gps(connection))
+        time.sleep(2.5)
+    # while True:
+    #     # Wait for the next MAVLink message
+    #     message = connection.recv_match(blocking=True, timeout=1)
+        
+    #     if message:
+    #         # Print the message
+    #         print(message.to_dict())
+    #     else:
+    #         print("No message received, retrying...")
+
 
     # Get current location
     #lat, lon, alt = get_latest_gps(connection)
