bf10cb71478f6270f7fef73e84b59bdc2586c2b1
Ben <benjaminparker@uvic.ca>
2025-03-24 17:22:45 -0700

    updates to cailbration function

--- src/tools/calibrate_camera.py
+++ src/tools/calibrate_camera.py
@@ -1,56 +1,71 @@
 import cv2 as cv
 import glob
 import numpy as np
+import sys
+import os
 
 def calibrate_camera():
 
-    # termination criteria
+    if len(sys.argv) != 2 or sys.argv[1] not in ['noir', 'wide']:
+        print("Usage: python3 script.py [noir|wide]")
+        sys.exit(1)
+
+    camera_type = sys.argv[1]
+
+    if camera_type == 'noir':
+        RESOLUTION = (3280, 2464)
+        IMAGE_DIR = os.path.expanduser('~/data/calibration_images/noir')
+    elif camera_type == 'wide':
+        RESOLUTION = (1280, 720)
+        IMAGE_DIR = os.path.expanduser('~/data/calibration_images/wide')
+
     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-    
-    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+
     objp = np.zeros((6*7,3), np.float32)
-    objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2) # units don't matter for our purposes
+    objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
     
-    # Arrays to store object points and image points from all the images.
-    objpoints = [] # 3d point in real world space
-    imgpoints = [] # 2d points in image plane.
+    objpoints = []
+    imgpoints = []
     
-    # List of paths to the calibration images
-    images = glob.glob("data/calibration_images/*.png")
-    
+    images = glob.glob(f"{IMAGE_DIR}/*.jpg")
+
+    if not images:
+        print(f"No images found in {IMAGE_DIR}")
+        sys.exit(1)
+
+    found_count = 0
     for fname in images:
         img = cv.imread(fname)
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    
-        # Find the chess board corners
+
         ret, corners = cv.findChessboardCorners(gray, (7,6), None)
-    
-        # If found, add object points, image points (after refining them)
-        if ret == True:
+
+        if ret:
+            found_count += 1
             objpoints.append(objp)
-    
-            corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+            corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
             imgpoints.append(corners2)
-    
-            # Draw and display the corners
+
             cv.drawChessboardCorners(img, (7,6), corners2, ret)
             cv.imshow('img', img)
             cv.waitKey(500)
+        else:
+            print(f"Chessboard not detected in image {fname}")
 
-    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-    
     cv.destroyAllWindows()
+
+    if found_count == 0:
+        print("No chessboards detected. Calibration failed.")
+        sys.exit(1)
+
+    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, RESOLUTION, None, None)
 
     return ret, mtx, dist, rvecs, tvecs
 
-    
-
 def main():
-    
     ret, mtx, dist, rvecs, tvecs = calibrate_camera()
-    print(f"ret:\n{ret}\nmtx:\n{mtx}\ndist:\n{dist}\nrvecs:\n{rvecs}\ntvecs:\n{tvecs}")
+    print(f"ret:\n{ret}\n\nmtx:\n{mtx}\n\ndist:\n{dist}\n\nrvecs:\n{rvecs}\n\ntvecs:\n{tvecs}")
     # Once run: copy/paste mtx and dist to camera_matrix and distortion_coeffs in get-image-points-gps
-    
 
 if __name__ == "__main__":
     main()
