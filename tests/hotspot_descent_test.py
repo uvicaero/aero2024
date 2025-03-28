d8eb1508ed30a8d7f142b90e527c553ce8eaabdb
Matthewhightech <Smoose007@gmail.com>
2025-03-28 12:32:20 -0700

    Add print

--- tests/hotspot_descent_test.py
+++ tests/hotspot_descent_test.py
@@ -114,6 +114,7 @@ def retrieve_gps():
     """
     global latest_gps, latest_attitude
 
+    print(f"Latest Gps: {latest_gps}")
     lat, lon, rel_alt, vx, vy, vz = latest_gps if latest_gps else (None, None, None, None, None, None)
 
     # Extract only pitch and yaw (ignore roll)
